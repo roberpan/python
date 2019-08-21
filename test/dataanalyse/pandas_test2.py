ca491c4812fb8cc60771d24cfa11f6d71399b0fe
roberpan <panlb1990@sina.com>
2019-08-21 20:49:13 +0800

    test

--- test/dataanalyse/pandas_test2.py
+++ test/dataanalyse/pandas_test2.py
@@ -14,8 +14,3 @@ df = pd.DataFrame(np.arange(25).reshape(5,5),
                        index=['Joe', 'Steve', 'Wes', 'Jim', 'Travis'])
 
 print(df)
-
-map={'a': 'red', 'b': 'red', 'c': 'blue', 'd': 'blue', 'e': 'red', 'f' : 'orange'}
-
-df1=df.groupby(map,axis=1).sum()
-print(df1)
